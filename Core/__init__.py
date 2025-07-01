f7f328b509cab8c7e65536af32e9bd0317236f51
Core Alcoser <corrinaalcoser@gmail.com>
2025-07-01 12:19:56 -0500

    re-added __init__.py

---
+++ Core/__init__.py
@@ -0,0 +1,6 @@
+# ============================================================================
+# core/__init__.py
+# Marks the core module for CHARLOTTE package imports
+# ============================================================================
+
+# Intentionally left empty to support package-level imports
